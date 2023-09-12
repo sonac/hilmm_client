df4dd1f17196fc79465b3d07c9d32186e8b73cb9
sonac <sonag007@gmail.com>
2023-09-12 09:27:48 +0200

    feat: adding remove asset

--- src/components/MainTable.tsx
+++ src/components/MainTable.tsx
@@ -9,6 +9,7 @@ import {
   TableCaption,
   TableContainer,
   Text,
+  Button,
 } from "@chakra-ui/react";
 import { UserPortfolio, UserAsset } from "../interfaces/user";
 import { snakeToCamel } from "../utils/camelize";
@@ -22,12 +23,28 @@ export default function MainTable(props: MainTableProps) {
     return <Text>Add some investment to see them below</Text>;
   }
 
+  const sellAsset = async (assetName: string) => {
+    const assetBody = {
+      asset_name: assetName,
+    };
+    await fetch(`http://localhost:8000/api/asset`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(assetBody),
+    });
+
+    location.assign("/");
+  };
+
   const portfolio: UserPortfolio = snakeToCamel(props.portfolio);
 
   return (
     <TableContainer>
       <Table variant="simple" colorScheme="gray" borderWidth="1px">
-        <TableCaption>Imperial to metric conversion factors</TableCaption>
+        <TableCaption>Portfolio</TableCaption>
         <Thead>
           <Tr>
             <Th borderWidth="1px">Asset Name</Th>
@@ -38,19 +55,34 @@ export default function MainTable(props: MainTableProps) {
             <Th borderWidth="1px" isNumeric>
               Current Value
             </Th>
+            <Th borderWidth="1px">Sell</Th>
           </Tr>
         </Thead>
         <Tbody>
           {portfolio.userAssets.map((ast: UserAsset) => {
             return (
-              <Tr>
-                <Td borderWidth="1px">{ast.asset.name}</Td>
-                <Td borderWidth="1px">{ast.asset.ticker}</Td>
-                <Td borderWidth="1px" isNumeric>
+              <Tr key={`${ast.asset.name}_row`}>
+                <Td borderWidth="1px" key={ast.asset.name}>
+                  {ast.asset.name}
+                </Td>
+                <Td borderWidth="1px" key={ast.asset.ticker}>
+                  {ast.asset.ticker}
+                </Td>
+                <Td borderWidth="1px" key={ast.asset.price} isNumeric>
                   {ast.asset.price}
                 </Td>
-                <Td borderWidth="1px" isNumeric>
+                <Td borderWidth="1px" isNumeric key={ast.currentValue}>
                   {ast.currentValue}
+                </Td>
+                <Td key={`${ast.asset.name}_key`}>
+                  <Button
+                    key={`${ast.asset.name}_button`}
+                    variant="solid"
+                    colorScheme="red"
+                    onClick={() => sellAsset(ast.asset.name)}
+                  >
+                    Sell
+                  </Button>
                 </Td>
               </Tr>
             );
